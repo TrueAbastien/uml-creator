0dd495968c80286be4a53241b54e90ccdbeade2f
Bastien Legoy <bastien.legoy@twin-robotics.com>
2021-10-10 16:32:05 +0200

    First Graphviz Class Diagram

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Graphviz setup !
 
 Useful Sources:
 [GVC Doc](https://graphviz.org/pdf/libguide.pdf)
+[GVC Attibutes](https://graphviz.org/doc/info/attrs.html)
--- src/tools/graphviz_test/sources/main.cpp
+++ src/tools/graphviz_test/sources/main.cpp
@@ -15,19 +15,24 @@ int main(int argc, char** argv)
     }
 
     // Create Graph
-    Agraph_t* graph = agopen("Test", Agdirected, nullptr);
+    Agraph_t* graph = agopen("Test", Agundirected, nullptr);
 
     // Graph Content
     {
+        // Defaults
+        agattr(graph, AGNODE, "shape", "record");
+        agattr(graph, AGEDGE, "dir", "both");
+
         // Nodes
         Agnode_t* node_1 = agnode(graph, "First", 1);
+        agsafeset(node_1, "label", "{First|...| + render()\\l...}", "");
         Agnode_t* node_2 = agnode(graph, "Second", 1);
+        agsafeset(node_2, "label", "{Second| - value : int\\l...| + render()\\l...}", "");
 
         // Edges
-        Agedge_t* edge_1 = agedge(graph, node_1, node_2, "label", 1);
-
-        // Attributes
-        agsafeset(node_1, "color", "blue", "");
+        Agedge_t* edge_1 = agedge(graph, node_1, node_2, nullptr, 1);
+        agsafeset(edge_1, "arrowtail", "empty", "");
+        agsafeset(edge_1, "arrowhead", "none", "");
     }
 
     // Setup 'Correct' Graph
